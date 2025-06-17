12cfb775ea8759bfd68b68c4b43b839d30aa70d3
fangfang <hyf781222@gmail.com>
2025-06-17 16:18:03 +0800

    feat: add lazy validation

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,10 +1,11 @@
 <template>
   <form @submit.prevent="onSubmit">
     <BaseInput
-      v-model="email"
       label="Email"
       type="email"
       :error="emailError"
+      :modelValue="email"
+      @change="handleChange"
     />
 
     <BaseInput
@@ -14,57 +15,47 @@
       :error="passwordError"
     />
 
-    <BaseButton
-      type="submit"
-      class="-fill-gradient"
-    >
-    Submit
-    </BaseButton>
+    <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
-<script>
-import { useField, useForm } from 'vee-validate'
-export default {
-  setup () {
-    function onSubmit () {
-      alert('Submitted')
-    }
+<script setup>
+import { useField, useForm } from "vee-validate";
 
-    const validations = {
-      email: value => {
-        if (!value) return 'This field is required'
-
-        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if (!regex.test(String(value).toLowerCase())) {
-          return 'Please enter a valid email address'
-        }
-
-        return true
-      },
-      password: value => {
-        const requiredMessage = 'This field is required'
-        if (value === undefined || value === null) return requiredMessage
-        if (!String(value).length) return requiredMessage
-
-        return true
-      }
-    }
-
-    useForm({
-      validationSchema: validations
-    })
-
-    const { value: email, errorMessage: emailError } = useField('email')
-    const { value: password, errorMessage: passwordError } = useField('password')
-
-    return {
-      onSubmit,
-      email,
-      emailError,
-      password,
-      passwordError
-    }
-  }
+function onSubmit() {
+  alert("Submitted");
 }
+
+const validations = {
+  email: (value) => {
+    if (!value) return "This field is required";
+
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regex.test(String(value).toLowerCase())) {
+      return "Please enter a valid email address";
+    }
+
+    return true;
+  },
+  password: (value) => {
+    const requiredMessage = "This field is required";
+    if (value === undefined || value === null) return requiredMessage;
+    if (!String(value).length) return requiredMessage;
+
+    return true;
+  },
+};
+
+const { setFieldValue } = useForm({
+  validationSchema: validations,
+});
+
+const handleChange = (event) => {
+  setFieldValue("email", event.target.value);
+};
+
+const { value: email, errorMessage: emailError } = useField("email");
+
+const { value: password, errorMessage: passwordError } = useField("password");
 </script>
